37df10b8131aaa99de6418d5dfda0d42ef31c9d1
cathalgking <32261323+cathalgking@users.noreply.github.com>
2020-05-01 21:41:09 +0100

    Add files via upload

---
+++ remove_entries_below_one.R
@@ -0,0 +1,12 @@
+# Remove values below 1 in a dataset. Removes a row that contain value(s) below 1 anywhere in that row. Reduces the matrix to one with less rows. 
+
+library(readr, lib.loc = "/usr/lib/R/site-library")
+
+df <- read_tsv("/home/cathal_king/Desktop/test_data.csv", na = "null")
+
+rows_above_one <- apply(df[,-1], 1, min) >1 
+
+df_above_one <- df[rows_above_one, ]
+
+write_tsv(df_above_one, path = "test_data_out.tsv")
+
