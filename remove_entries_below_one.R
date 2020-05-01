fb010e2f8fc25127ebd35e8745fd85b16f892b80
cathalgking <32261323+cathalgking@users.noreply.github.com>
2020-05-01 21:44:00 +0100

    Update remove_entries_below_one.R

--- remove_entries_below_one.R
+++ remove_entries_below_one.R
@@ -1,6 +1,6 @@
 # Remove values below 1 in a dataset. Removes a row that contain value(s) below 1 anywhere in that row. Reduces the matrix to one with less rows. 
 
-library(readr, lib.loc = "/usr/lib/R/site-library")
+library(readr)
 
 df <- read_tsv("/home/cathal_king/Desktop/test_data.csv", na = "null")
 
