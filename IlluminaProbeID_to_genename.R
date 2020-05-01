cbd8226186291959c019786b016ef1aeee2948b1
cathalgking <32261323+cathalgking@users.noreply.github.com>
2020-05-01 20:58:05 +0100

    Add files via upload

---
+++ IlluminaProbeID_to_genename.R
@@ -0,0 +1,15 @@
+## Convert a list of illumina probes to gene names
+
+# load the illumina probe library for mouse. Change for whatever species in question. 
+library(illuminaMousev2.db, lib.loc = "/usr/lib/R/site-library")
+
+# read in probe ID list to convert
+probes <- read.table("/home/cathal_king/Desktop/illumina_probes.txt", header = F, stringsAsFactors = F)
+
+probes <- probes[[1]]
+
+df <- data.frame(Gene=unlist(mget(x = probes,envir = illuminaMousev2SYMBOLREANNOTATED)))
+
+# will output a 2 column dataset containing the probeID and corresponding gene name
+write.table(df, file = "probe_conversion.txt", sep = "\t", row.names = T)
+
